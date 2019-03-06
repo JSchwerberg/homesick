c154e8859a2a7e9d850e08afbb9ec9d8f8a971c9
Jason Schwerberg <jason@schwerberg.com>
2019-03-05 22:47:19 -0800

    Add ZSH Colorize Style environment variable
    Pull request to get this into mainline oh-my-zsh is at
    https://github.com/robbyrussell/oh-my-zsh/pull/7635

--- dotfiles/.env_vars.sh
+++ dotfiles/.env_vars.sh
@@ -3,4 +3,4 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/dotnet"
 export EDITOR="nvim"
 export BROWSER="firefox"
-export TESTENV="true"
+export ZSH_COLORIZE_STYLE="paraiso-dark"
